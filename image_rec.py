30600ba23b45d466dd7b5fe09a4026bf7ce278b8
Jaime <jaimercarave@gmail.com>
2020-12-31 00:23:51 +0100

    working_camera

--- image_rec.py
+++ image_rec.py
@@ -1,12 +1,6 @@
 import numpy as np
 import cv2
 from tensorflow.keras.models import load_model
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 
 emotion_model_path = '_mini_XCEPTION_5_FER_RAF_CK.hdf5'
@@ -14,16 +8,16 @@ model = load_model(emotion_model_path, compile=False)
 
 num_emotions=5
 emotion_dict = {0: "Neutral", 1: "Felicidad", 2: "Sorpresa", 3: "Tristeza", 4: "Enfado"}
-
+facecasc = cv2.CascadeClassifier('haarcascade_files/haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS,5)
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    facecasc = cv2.CascadeClassifier('haarcascade_files\haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    canvas = np.zeros((250, 300, 3), dtype="uint8")
+    #canvas = np.zeros((250, 300, 3), dtype="uint8")
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
@@ -33,7 +27,7 @@ while True:
         maxindex = int(np.argmax(prediction))
         cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
                     cv2.LINE_AA)
-        for (i, (emotion, prob)) in enumerate(zip(emotion_dict.values(), prediction)):
+        """for (i, (emotion, prob)) in enumerate(zip(emotion_dict.values(), prediction)):
             # construct the label text
             text = "{}: {:.2f}%".format(emotion, prob * 100)
 
@@ -45,10 +39,10 @@ while True:
                           (w, (i * 35) + 35), (0, 0, 255), -1)
             cv2.putText(canvas, text, (10, (i * 35) + 23),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.45,
-                        (255, 255, 255), 2)
+                        (255, 255, 255), 2)"""
 
-    cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
-    cv2.imshow("Probabilities", canvas)
+    cv2.imshow('Video', cv2.resize(frame, (640,480), interpolation=cv2.INTER_CUBIC))
+    #cv2.imshow("Probabilities", canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
