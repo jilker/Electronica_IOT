955b1d8720675e4dc9834542ef99d67d68f7c6b4
Jaime <jaimercarave@gmail.com>
2020-12-30 22:31:27 +0100

    first_attemp_picamera

--- image_rec.py
+++ image_rec.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 from tensorflow.keras.models import load_model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
@@ -8,30 +10,39 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
+import time
 
 emotion_model_path = '_mini_XCEPTION_5_FER_RAF_CK.hdf5'
 model = load_model(emotion_model_path, compile=False)
 
 num_emotions=5
 emotion_dict = {0: "Neutral", 1: "Felicidad", 2: "Sorpresa", 3: "Tristeza", 4: "Enfado"}
+facecasc = cv2.CascadeClassifier('haarcascade_files\haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    facecasc = cv2.CascadeClassifier('haarcascade_files\haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	image = frame.array
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     canvas = np.zeros((250, 300, 3), dtype="uint8")
 
+	cv2.imshow("Frame", image)
+
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        cv2.rectangle(image, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        prediction = model.predict(cropped_img/255)[0]
+        prediction = model.predict(cropped_img / 255)[0]
         maxindex = int(np.argmax(prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+        cv2.putText(image, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
                     cv2.LINE_AA)
         for (i, (emotion, prob)) in enumerate(zip(emotion_dict.values(), prediction)):
             # construct the label text
@@ -47,9 +58,11 @@ while True:
                         cv2.FONT_HERSHEY_SIMPLEX, 0.45,
                         (255, 255, 255), 2)
 
-    cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
+    cv2.imshow('Video', cv2.resize(image, (640,480), interpolation=cv2.INTER_CUBIC))
     cv2.imshow("Probabilities", canvas)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
