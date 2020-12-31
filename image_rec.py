4aa230883277c41864cf5e4a00a940be8ca75130
Jaime <jaimercarave@gmail.com>
2020-12-31 02:24:49 +0100

    working_but_slow

--- image_rec.py
+++ image_rec.py
@@ -2,15 +2,33 @@ import numpy as np
 import cv2
 from tensorflow.keras.models import load_model
 import os
+import time
 
 emotion_model_path = '_mini_XCEPTION_5_FER_RAF_CK.hdf5'
 model = load_model(emotion_model_path, compile=False)
 
 num_emotions=5
 emotion_dict = {0: "Neutral", 1: "Felicidad", 2: "Sorpresa", 3: "Tristeza", 4: "Enfado"}
+list_emotion = {"Neutral":0 , "Felicidad":0 , "Sorpresa":0 , "Tristeza":0 , "Enfado":0}
 facecasc = cv2.CascadeClassifier('haarcascade_files/haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS,5)
+cont=0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    tic=time.perf_counter()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)        
+    toc=time.perf_counter()
+    print(f"Predicción {toc-tic:0.4f}seconds")
+    cv2.imshow('Video', cv2.resize(frame, (640,480), interpolation=cv2.INTER_CUBIC))
+    #cv2.imshow("Probabilities", canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()    
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -18,15 +36,21 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     #canvas = np.zeros((250, 300, 3), dtype="uint8")
-
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        #cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        cont += 1
         roi_gray = gray[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        tic=time.perf_counter()
         prediction = model.predict(cropped_img/255)[0]
+        toc=time.perf_counter()
+        print(f"Predicción {toc-tic:0.4f}seconds")
         maxindex = int(np.argmax(prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
-                    cv2.LINE_AA)
+        list_emotion[emotion_dict.get(maxindex)]+=1
+        if cont==100:
+            print(list_emotion.values())
+            cont=0        
+        #cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,cv2.LINE_AA)
         """for (i, (emotion, prob)) in enumerate(zip(emotion_dict.values(), prediction)):
             # construct the label text
             text = "{}: {:.2f}%".format(emotion, prob * 100)
@@ -41,7 +65,7 @@ while True:
                         cv2.FONT_HERSHEY_SIMPLEX, 0.45,
                         (255, 255, 255), 2)"""
 
-    cv2.imshow('Video', cv2.resize(frame, (640,480), interpolation=cv2.INTER_CUBIC))
+    #cv2.imshow('Video', cv2.resize(frame, (640,480), interpolation=cv2.INTER_CUBIC))
     #cv2.imshow("Probabilities", canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
